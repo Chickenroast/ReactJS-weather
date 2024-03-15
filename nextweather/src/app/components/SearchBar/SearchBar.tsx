32c9fc69c9116f3341e22b21101bb2fe85242070
Julia Michel T. <juliamicheltex@gmail.com>
2024-03-15 14:33:04 +0100

    adding props need to have a props fiels with icon can be used everywhere

--- nextweather/src/app/components/SearchBar/SearchBar.tsx
+++ nextweather/src/app/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,28 @@
 "use client";
 
 import { useState } from "react";
+import { HiSearch } from "react-icons/hi";
+
+interface SearchBarProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  Icon?: React.ReactNode;
+}
+
+const SearchBarProps: React.FC<SearchBarProps> = ({ Icon, ...props }) => {
+  return (
+    <div className="relative flex items-center text-black">
+      {Icon && (
+        <div className="absolute inset-y-0 left-0 pl-2 flex items-center">
+          {Icon}
+        </div>
+      )}
+      <input
+        type="text"
+        className={`w-full py-2 pl-10 pr-4 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent`}
+        {...props}
+      />
+    </div>
+  );
+};
 
 export function SearchBar() {
   const [searchInput, setSearchInput] = useState("");
@@ -26,14 +48,15 @@ export function SearchBar() {
 
   return (
     <main>
-      <input
-        type="text"
-        className="text-black rounded-md p-1 m-2 w-60"
-        onChange={handleChange}
-        value={searchInput}
-        placeholder="Search for a country"
-      />
-      <table className="text-black rounded-md p-1 bg-white m-2 w-60">
+      <div>
+        <SearchBarProps
+          onChange={handleChange}
+          value={searchInput}
+          placeholder="Search for a country"
+          Icon={<HiSearch />}
+        />
+      </div>
+      <table className="text-black rounded-md py-2 pl-10 pr-4 bg-white m-2 w-full">
         <tr>
           <th className="p-1">
             <p className="text-left">Result</p>
