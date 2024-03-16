141dfb8e9e4fbb379269951c921b770bca39a08c
Julia Michel T. <juliamicheltex@gmail.com>
2024-03-16 08:01:19 +0100

    fetch all data is working

--- nextweather/src/app/components/SearchBar/SearchBar.tsx
+++ nextweather/src/app/components/SearchBar/SearchBar.tsx
@@ -2,27 +2,7 @@
 
 import { useState } from "react";
 import { HiSearch } from "react-icons/hi";
-
-interface SearchBarProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  Icon?: React.ReactNode;
-}
-
-const SearchBarProps: React.FC<SearchBarProps> = ({ Icon, ...props }) => {
-  return (
-    <div className="relative flex items-center text-black">
-      {Icon && (
-        <div className="absolute inset-y-0 left-0 pl-2 flex items-center">
-          {Icon}
-        </div>
-      )}
-      <input
-        type="text"
-        className={`w-full py-2 pl-10 pr-4 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent`}
-        {...props}
-      />
-    </div>
-  );
-};
+import { Field } from "../Fields/Field";
 
 export function SearchBar() {
   const [searchInput, setSearchInput] = useState("");
@@ -49,7 +29,7 @@ export function SearchBar() {
   return (
     <main>
       <div>
-        <SearchBarProps
+        <Field
           onChange={handleChange}
           value={searchInput}
           placeholder="Search for a country"
--- nextweather/src/app/page.tsx
+++ nextweather/src/app/page.tsx
@@ -1,9 +1,12 @@
+import WeatherComponent from "./components/Weather/Weather";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="bg-black">
         <h1 className="text-white text-4xl">Weather App</h1>
       </div>
+      <WeatherComponent />
     </main>
   );
 }
